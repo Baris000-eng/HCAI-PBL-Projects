51f4249f785ea2580e07bade9b94334dd02c4d16
Baris000-eng <you@email.com>
2026-09-05 13:48:19 +0300

    changed lowercase=False to True in count vectorizer

--- project4/views.py
+++ project4/views.py
@@ -17,7 +17,7 @@ def load_and_process_local_dataset(file_path=file_path):
     df = dataset['train'].to_pandas()
     df = df.dropna(subset=['movie_title', 'genres', 'imdb_score'])
     
-    cv = CountVectorizer(tokenizer=lambda text: text.split('|'), token_pattern=None, lowercase=False)
+    cv = CountVectorizer(tokenizer=lambda text: text.split('|'), token_pattern=None, lowercase=True)
     genre_matrix = cv.fit_transform(df['genres']).toarray()
     
     scaler = MinMaxScaler()
